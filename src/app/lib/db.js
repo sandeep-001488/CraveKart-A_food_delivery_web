3a5ac6481f01d6e45e8c979ccf87cd232f6651bc
sandeep <sandeepsanu1230@gmail.com>
2024-10-18 02:52:21 +0530

    updated  connection

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,6 +1,5 @@
-const { USERNAME, PASSWORD } = process.env;
+require("dotenv").config();
 
-// export const connectDB="mongodb+srv://"+USERNAME+":"+PASSWORD+"@cluster0.hcf5r.mongodb.net/goFood-db?retryWrites=true&w=majority&appName=Cluster0";
+const { USERNAME, PASSWORD, DB_NAME } = process.env;
 
-export const connectDB =
-  "mongodb+srv://sandeepsanu1230:mongo1230@cluster0.hcf5r.mongodb.net/goFood-db?retryWrites=true&w=majority&appName=Cluster0";
+export const connectDB = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.hcf5r.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
